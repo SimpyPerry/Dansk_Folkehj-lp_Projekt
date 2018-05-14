bef9774e12c35a1891964e2d6f69b42ea2ed7a34
Alice Duong <alic0579@edu.eal.dk>
2018-05-14 09:39:52 +0200

    B

--- Dansk_Folkehjælp_Projekt/Models/DatabaseConnection.cs
+++ Dansk_Folkehjælp_Projekt/Models/DatabaseConnection.cs
@@ -14,7 +14,7 @@ namespace Dansk_Folkehjælp_Projekt.Models
             //Forbinder til databasen
             public ObservableCollection <Storage> GetStorages { get; set; }
             private static string connectionString = "Server=EALSQL1.eal.local; Database= DB2017_A21; User ID = USER_A21; Password=SesamLukOp_21;";
-            public List<string> bookcases { get; set; }
+            public List<string> Bookcases { get; set; }
             public ObservableCollection<Storage> GetBags { get; set; }
             public ObservableCollection<Storage> GetItems { get; set; }
 
